cb5cc8157983fad92a7ec2db47f09ec9ab2adf42
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 22:55:03 +0000

    Client server connection established. userInput function can send write data to server, with conn object argument

--- play.js
+++ play.js
@@ -1,8 +1,5 @@
-const {connect} = require('../snake-client/client') //imports connection functionality, initializes connection to server local host address, and then matching server port
-const {setupInput} = require('./input') //imports user input functionality
+const {connect} = require('../snake-client/client'); //imports connection functionality, initializes connection to server local host address, and then matching server port
+const {setupInput} = require('./input'); //imports user input functionality
 console.log("Connecting ..."); //log that the file is being read, and is technically moving to the connection stage
-connect(); //call on the connect function
-
-process.stdout.write('\x07'); //user system sound to let user know connected to server
-
-setupInput(); //call on user input function
+const conn = connect(); //set connect function to conn variable
+setupInput(conn); //pass the object return by connection in to the setupInput function as an argumetn
