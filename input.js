8d911cc3e2393253808a7dd1dc3f545e713531ae
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 22:56:03 +0000

    User input logic using wasd keys for movement. Uses conn object from connect function to write data to connection

--- input.js
+++ input.js
@@ -1,22 +1,33 @@
-const setupInput = function () {
+let connection; //initialize global empty object
+
+const setupInput = function(conn) { //take in the conn object as an argument to UserInputFunction (so this function knows where to write/send user input)
+  connection = conn; //global connection object now imported conn argument from client.js
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", key => {
-    handleUserInput(key)
-  });
+  stdin.on("data", handleUserInput); //on input from the user or "data", call the function handleUserInput, which then write the user output to the server
   return stdin;
 };
 
 const handleUserInput = (key) => {
-  const stdout = process.stdout;
   if (key === '\u0003') {
-    stdout.write("Goodbye now!\n");
-    process.exit()
+    process.exit();
   }
-}
+  if (key === 'w') {
+    connection.write("Move: up"); //sends a client write to server connection (conn object -- server reads the data "Move: up" and in turn moves the user 1 unit up)
+  }
+  if (key === 's') {
+    connection.write("Move: down"); //all movement logic below is the same as above
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
+};
 
-module.exports = {
+module.exports = { //export the function as an object to be used in other files
   setupInput: setupInput,
-}
+};
