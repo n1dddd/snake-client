9524f3c93ce758009aefd6663aefedd1a12ca3d9
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 22:53:59 +0000

    Client-server connection established. Pass connection argument to input function. Input is functional

--- client.js
+++ client.js
@@ -5,17 +5,14 @@ const connect = function() {
     host: 'localhost', // IP address here,
     port: 50541 // PORT number here,
   });
+  conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: DS"); 
+    conn.write("Name: DS");
   });
-  // interpret incoming data as text
-
-  conn.setEncoding("utf8");
-
   conn.on("data", (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
   return conn;
 };
 
