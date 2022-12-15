cf3cb43a5b53f4448811503c16ede054c34cafa7
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 23:39:39 +0000

    Import movement logic from constants.js, update function logic and references. Lint file

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const {MOVE_DOWN_KEY,MOVE_UP_KEY,MOVE_LEFT_KEY,MOVE_RIGHT_KEY,messages} = require('./constants'); //import movement logic from constants file, store within object
+
 let connection; //initialize global empty object
 
 const setupInput = function(conn) { //take in the conn object as an argument to UserInputFunction (so this function knows where to write/send user input)
@@ -14,17 +16,23 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) { //if user input key matches the key from constants.js
     connection.write("Move: up"); //sends a client write to server connection (conn object -- server reads the data "Move: up" and in turn moves the user 1 unit up)
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down"); //all movement logic below is the same as above
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 'd') {
-    connection.write("Move: right")
+  if (key === MOVE_RIGHT_KEY) {
+    connection.write("Move: right");
+  }
+  if (messages[key]) {
+    connection.write(`Say: ${messages[key]}`);
+  }
+  if (key === 't') {
+    connection.write("Say: secret message unlocked");
   }
 };
 
