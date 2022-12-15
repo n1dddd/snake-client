ab8a05fb30c4b0bd46ba580ac3406b3388cf3800
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 02:21:19 +0000

    Name of player is wrote from client to server.

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost', // IP address here,
     port: 50541 // PORT number here,
   });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: DS");
+  });
   conn.on("data", (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   return conn;
