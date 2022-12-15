6576a7c731b5550438af6a34ee3a4cc69383c63c
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 23:41:10 +0000

    Final lint

--- play.js
+++ play.js
@@ -2,4 +2,4 @@ const {connect} = require('../snake-client/client'); //imports connection functi
 const {setupInput} = require('./input'); //imports user input functionality
 console.log("Connecting ..."); //log that the file is being read, and is technically moving to the connection stage
 const conn = connect(); //set connect function to conn variable
-setupInput(conn); //pass the object return by connection in to the setupInput function as an argumetn
+setupInput(conn); //pass the object return by connection in to the setupInput function as an argument
