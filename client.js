53edd50c3973994513a217f82ca98a73573e423f
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 23:42:06 +0000

    Import IP and PORT from constants.js. Final lint

--- client.js
+++ client.js
@@ -1,21 +1,22 @@
 const net = require("net");
+const {IP,PORT} = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here,
-    port: 50541 // PORT number here,
+    host: IP,
+    port:PORT
   });
   conn.setEncoding("utf8");
-  conn.on("connect", () => {
+  conn.on("connect", () => { //when a connection is established, this function is called immediately
     console.log("Successfully connected to game server");
     conn.write("Name: DS");
   });
   conn.on("data", (data) => {
     console.log(data);
   });
-  return conn;
+  return conn; //return the conn object
 };
 
-module.exports = {
+module.exports = { //export the function
   connect: connect
 };
