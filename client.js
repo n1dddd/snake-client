a9034a953e1ff26b4079e95b861fd9c0a8bfcc3d
Daniel Seniv <danielseniv@outlook.com>
2022-12-15 03:03:02 +0000

    Variations of writing data from client to server

--- client.js
+++ client.js
@@ -8,11 +8,22 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: DS");
-  });
-  conn.on("data", (data) => {
-    console.log(data);
+    // Set timeout code, sending raw conn.write in succession did not work
+    // setTimeout(() => { //Using timeOut enables movement
+    //   conn.write("Move: up");
+    // },50)
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // },100)
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // },150)
+    setInterval(() => {
+      conn.write("Move: up");
+    },300);
   });
   // interpret incoming data as text
+
   conn.setEncoding("utf8");
   return conn;
 };
